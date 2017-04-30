950ec65673230e3ea76d5fc6eb7312767888980a
Tubakien <tubakien@gmail.com>
2017-04-30 17:01:15 -0500

    Added UserRole from Hibernate.

---
+++ src/main/java/com/entity/UserRole.java
@@ -0,0 +1,66 @@
+package com.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by student on 4/30/17.
+ */
+@Entity
+@Table(name = "user_role", schema = "ride_share2", catalog = "")
+public class UserRole {
+    private String username;
+    private String roleName;
+    private int roleId;
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "role_name")
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Id
+    @Column(name = "role_id")
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRole userRole = (UserRole) o;
+
+        if (roleId != userRole.roleId) return false;
+        if (username != null ? !username.equals(userRole.username) : userRole.username != null) return false;
+        if (roleName != null ? !roleName.equals(userRole.roleName) : userRole.roleName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (roleName != null ? roleName.hashCode() : 0);
+        result = 31 * result + roleId;
+        return result;
+    }
+}
