7c4db876f2e0f3f97c2ed8b7b592e846407175dd
Tubakien <tubakien@gmail.com>
2017-05-01 18:46:45 -0500

    Refactored address table. Added requestRide.jsp

--- src/main/java/com/entity/Address.java
+++ src/main/java/com/entity/Address.java
@@ -1,23 +1,23 @@
 package com.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
 
 /**
- * Created by student on 4/30/17.
+ * Created by student on 5/1/17.
  */
 @Entity
 @Table(name="address")
 public class Address {
     private int addressId;
-    private String address;
     private byte isBusiness;
     private String businessName;
+    private int addressNumber;
+    private String streetName;
+    private String city;
+    private String state;
+    private String zipCode;
 
     @Id
-    @GeneratedValue(generator = "increment")
-    @GenericGenerator(name="increment", strategy = "increment")
     @Column(name = "address_id")
     public int getAddressId() {
         return addressId;
@@ -25,16 +25,6 @@ public class Address {
 
     public void setAddressId(int addressId) {
         this.addressId = addressId;
-    }
-
-    @Basic
-    @Column(name = "address")
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     @Basic
@@ -57,18 +47,72 @@ public class Address {
         this.businessName = businessName;
     }
 
+    @Basic
+    @Column(name = "address_number")
+    public int getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(int addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    @Basic
+    @Column(name = "street_name")
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    @Basic
+    @Column(name = "city")
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Basic
+    @Column(name = "state")
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Basic
+    @Column(name = "zip_code")
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Address address1 = (Address) o;
+        Address address = (Address) o;
 
-        if (addressId != address1.addressId) return false;
-        if (isBusiness != address1.isBusiness) return false;
-        if (address != null ? !address.equals(address1.address) : address1.address != null) return false;
-        if (businessName != null ? !businessName.equals(address1.businessName) : address1.businessName != null)
+        if (addressId != address.addressId) return false;
+        if (isBusiness != address.isBusiness) return false;
+        if (addressNumber != address.addressNumber) return false;
+        if (businessName != null ? !businessName.equals(address.businessName) : address.businessName != null)
             return false;
+        if (streetName != null ? !streetName.equals(address.streetName) : address.streetName != null) return false;
+        if (city != null ? !city.equals(address.city) : address.city != null) return false;
+        if (state != null ? !state.equals(address.state) : address.state != null) return false;
+        if (zipCode != null ? !zipCode.equals(address.zipCode) : address.zipCode != null) return false;
 
         return true;
     }
@@ -76,9 +120,13 @@ public class Address {
     @Override
     public int hashCode() {
         int result = addressId;
-        result = 31 * result + (address != null ? address.hashCode() : 0);
         result = 31 * result + (int) isBusiness;
         result = 31 * result + (businessName != null ? businessName.hashCode() : 0);
+        result = 31 * result + addressNumber;
+        result = 31 * result + (streetName != null ? streetName.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (zipCode != null ? zipCode.hashCode() : 0);
         return result;
     }
 }
---
+++ src/main/webapp/requestRide.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Kien Warren
+  Date: 5/1/17
+  Time: 6:35 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Request a Ride</title>
+    <c:import url="theme/bootstrapReferences.jsp" />
+</head>
+<body>
+    <!-- Navigation -->
+    <c:import url="theme/nav.jsp" />
+
+    <h3>Ride Request Form</h3>
+    <form action="">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" >
+        <label for=""></label>
+    </form>
+</body>
+</html>
